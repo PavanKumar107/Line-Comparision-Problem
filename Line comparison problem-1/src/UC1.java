9b5e0c32399d2d07a38d9a1e6546154e16a64ad7
PavanKumar107 <99305975+PavanKumar107@users.noreply.github.com>
2022-03-12 15:36:43 +0530

    UC1

---
+++ Line comparison problem-1/src/UC1.java
@@ -0,0 +1,17 @@
+
+public class UC1 {
+
+	public static void main(String[] args) {
+		 System.out.println("Welcome to Line Comparsion program");
+
+	    	int x1,x2,y1,y2;
+			int  l1;
+			x1=3; 
+			y1=4;
+			x2=5;
+			y2=9;
+			l1=(int)Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+			System.out.println(l1);
+	}
+
+}
